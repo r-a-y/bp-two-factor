d12988e44a130b44754cd61de0ed4982d569b443
r-a-y <r-a-y@gmx.com>
2026-01-27 00:24:21 -0800

    CSS and JS updates for two-factor v0.12.0.
    In 2FA v0.12.0, the settings interface was simplified to remove the
    "Primary" radio button for each provider and to add a new "Primary
    Method" field instead. The following adjustments have been made:
    
    - Only show the "Primary Method" select dropdown if more than one 2FA
      provider has been configured by the user
    - For the "Primary Method" select dropdown, hide providers that have
      not been configured by the user. Always hide the Recovery Codes
      provider from the dropdown as well.
    - Remove older code referencing the older "Primary" radio button UI.
    - Add a 'two-factor-revalidate' CSS class to the Revalidate button.

--- assets/settings.js
+++ assets/settings.js
@@ -3,10 +3,11 @@ jQuery(function($){
 	var $u2fCheckbox = $( '#two-factor-options input[value="Two_Factor_FIDO_U2F"]' );
 	var $checkboxes = $( '.two-factor-methods-table input[type="checkbox"]' );
 	var $backupCodes = $( '#two-factor-backup-codes' );
-	var $backupCodesPrimary = $( '#two-factor-options input[type="radio"][value="Two_Factor_Backup_Codes"]' );
 	var $totp = $( '#two-factor-totp-options' );
 	var $webAuthnCheckbox = $( '#two-factor-options input[value="TwoFactor_Provider_WebAuthn"]' );
 	var $securityKeysWebAuthn = $('#webauthn-security-keys-section');
+	var $revalidateButton = $( '.two-factor-warning-revalidate-session .button' );
+	var $primaryMethods = $( '#two-factor-options .two-factor-primary-method-table select option:not(:disabled,[value=""],[value="Two_Factor_Backup_Codes"])' );
 
 	// Only show Security Keys section if checked.
 	if ( $u2fCheckbox.prop( 'checked' ) ) {
@@ -32,9 +33,6 @@ jQuery(function($){
 		}
 	} );
 
-	// Disable Recovery Codes primary radio button.
-	$backupCodesPrimary.prop( 'disabled', true );
-
 	// Eek. Inject strings and other stuff.
 	$securityKeys.find( '.register-security-key' ).prepend( bp2fa.security_key_desc );
 	$securityKeysWebAuthn.find( '.add-webauthn-key' ).prepend( bp2fa.security_key_webauthn_desc );
@@ -48,6 +46,14 @@ jQuery(function($){
 		$backupCodes.parent().prepend( bp2fa.backup_codes_generate );
 	}
 	$backupCodes.parent().prepend( bp2fa.recovery_codes_desc );
+
+	if ( $revalidateButton.length ) {
+		$revalidateButton.addClass( 'two-factor-revalidate' );
+	}
+
+	if ( $primaryMethods.length < 2  ) {
+		$( '.two-factor-primary-method-table, .two-factor-methods-table + hr' ).hide();
+	}
 
 	// Customizations for TOTP provider.
 	function totp_toggler() {
@@ -70,39 +76,31 @@ jQuery(function($){
 		}
 	} );
 
-	$checkboxes.on( 'change', function() {
-		var radio = $( 'input[name="_two_factor_provider"]' ),
-			radioVal = radio.filter( ':checked' ).val(),
-			checkPrimary = false;
-
-		// Enabled a provider.
-		if ( $(this).prop( 'checked' ) ) {
-			// Primary is Backup codes and Enabled is anything but Backup Codes.
-			if ( 'Two_Factor_Backup_Codes' !== $(this).val() && 'Two_Factor_Backup_Codes' === radioVal ) {
-				checkPrimary = true;
-
-			// No primary and Enabled is anything but Backup Codes.
-			} else if ( ! radioVal &&  'Two_Factor_Backup_Codes' !== $(this).val() ) {
-				checkPrimary = true;
-			}
-
-			// Check corresponding Primary if allowed.
-			if ( checkPrimary ) {
-				$(this).parent().next().find('input').prop( 'checked', true );
-			}
-
-		// Disabled a provider.
+	function backupCodes_toggler( providers ) {
+		// If no providers checked, hide Recovery Codes option.
+		if ( 0 === providers.length ) {
+			$backupCodes.parents('tr').hide();
 		} else {
-			// Deselect Primary provider for unchecked provider.
-			if ( radioVal === $(this).val() ) {
-				$( 'table.two-factor-methods-table input[type="radio"][value="' + $(this).val() + '"]' ).prop( 'checked', false );
+			$backupCodes.parents('tr').show();
+		}
+	}
+
+	backupCodes_toggler( $checkboxes.filter( ':checked' ) );
+
+	$checkboxes.on( 'change', function() {
+		var checked = $checkboxes.filter( ':checked' );
+
+		backupCodes_toggler( checked );
+
+		// Unchecked a provider.
+		if ( ! $(this).prop( 'checked' ) ) {
+			if ( ! checked.length ) {
+				return;
 			}
 
-			checked = $checkboxes.filter( ':checked' );
-
-			// Set a fallback primary provider that isn't Backup Codes.
-			if ( checked.length && 'Two_Factor_Backup_Codes' !== checked.first().val() ) {
-				$( 'table.two-factor-methods-table input[type="radio"][value="' + checked.first().val() + '"]' ).prop( 'checked', true );
+			// Uncheck Recovery Codes if it is the only provider remaining.
+			if ( 'Two_Factor_Backup_Codes' === checked.first().val() && checked.length === 1 ) {
+				$( 'table.two-factor-methods-table input[type="checkbox"][value="' + checked.first().val() + '"]' ).prop( 'checked', false );
 			}
 		}
 	} );
